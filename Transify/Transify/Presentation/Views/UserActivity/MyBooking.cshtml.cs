1037d0942a30427e963da3b98a94657539483bec
Blend06 <bk67418@ubt-uni.net>
2025-01-07 23:54:12 +0100

    krijimi i Views folder edhe elementeve per Front

---
+++ Transify/Transify/Presentation/Views/UserActivity/MyBooking.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Transify.Views.UserActivity
+{
+    public class MyBookingsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

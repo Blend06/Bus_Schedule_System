67e05df975fd83b36f807dfdaeee7c38eaf97678
GretaGashi <gg68557@ubt-uni.net>
2025-02-07 02:52:25 +0100

    Added Controller for Business Bus Controller

--- Transify/Transify/obj/Debug/net8.0/Transify.AssemblyInfo.cs
+++ Transify/Transify/obj/Debug/net8.0/Transify.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Transify")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efa17b86a2232951ead9279fa48b6130c5ecb48e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecd0c53dc6c9abeee10a1b4a49573ccdc44e9023")]
 [assembly: System.Reflection.AssemblyProductAttribute("Transify")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Transify")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

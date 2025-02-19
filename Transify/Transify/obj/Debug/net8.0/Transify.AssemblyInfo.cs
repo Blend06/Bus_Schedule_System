5b09f820f2002502c131c11674fa10a2347f8955
oltahasimja <149635414+oltahasimja@users.noreply.github.com>
2025-02-19 22:04:06 +0100

    addcompany.js
    json for adding a bus company

--- Transify/Transify/obj/Debug/net8.0/Transify.AssemblyInfo.cs
+++ Transify/Transify/obj/Debug/net8.0/Transify.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Transify")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dba1f72b3d06ddf7d991ce134ba131bf90170649")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12031e51ca982b0fd19d748785357a148f1f314c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Transify")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Transify")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

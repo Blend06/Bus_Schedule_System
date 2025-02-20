abc35877df60fc17f8b08746d0bee6e49b63118c
GretaGashi <gg68557@ubt-uni.net>
2025-02-20 21:00:37 +0100

    Bus Schedule Search json file

--- Transify/Transify/obj/Debug/net8.0/Transify.AssemblyInfo.cs
+++ Transify/Transify/obj/Debug/net8.0/Transify.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Transify")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43ae19fa4a4f9e834c5905c8cb0537d27c5f306b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d9f2cba9b49c29d96c30de83da1cf229fa870ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("Transify")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Transify")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

0631340056ec8cb9071def56e12f0e6c57018ebd
oltahasimja <149635414+oltahasimja@users.noreply.github.com>
2025-02-21 12:29:13 +0100

    Taxi Company Controller

--- Transify/Transify/obj/Release/net8.0/Transify.AssemblyInfo.cs
+++ Transify/Transify/obj/Release/net8.0/Transify.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Transify")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abc35877df60fc17f8b08746d0bee6e49b63118c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+943a2b591b9c7a74192fe09398547411bc2939a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Transify")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Transify")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

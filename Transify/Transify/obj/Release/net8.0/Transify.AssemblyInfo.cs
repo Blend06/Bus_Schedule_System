5da548a7f303a421d8b9d3b9d45d666527b0df22
GretaGashi <gg68557@ubt-uni.net>
2025-02-24 23:48:40 +0100

    TaxiManagementController

--- Transify/Transify/obj/Release/net8.0/Transify.AssemblyInfo.cs
+++ Transify/Transify/obj/Release/net8.0/Transify.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Transify")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e0d4c3cc2e787aa0585198963721912c1003b38")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a693806bbfc8c1b163addb59cd867554199eb0e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Transify")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Transify")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

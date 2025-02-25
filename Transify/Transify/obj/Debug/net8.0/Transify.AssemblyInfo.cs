4dd44e290d8fc62bab430558267f6c3086527b66
oltahasimja <149635414+oltahasimja@users.noreply.github.com>
2025-02-25 22:01:33 +0100

    TaxiBooking && TaxiReservations json

--- Transify/Transify/obj/Debug/net8.0/Transify.AssemblyInfo.cs
+++ Transify/Transify/obj/Debug/net8.0/Transify.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Transify")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5da548a7f303a421d8b9d3b9d45d666527b0df22")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7011a7b0d69fc3bd1a273a59208c57bf427088ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("Transify")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Transify")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

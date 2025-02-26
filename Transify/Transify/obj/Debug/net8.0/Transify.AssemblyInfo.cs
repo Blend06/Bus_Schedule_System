a05fe36b41c26aadd197c605e2e262545b08e691
GretaGashi <gg68557@ubt-uni.net>
2025-02-26 20:59:46 +0100

    DriverDashboard json

--- Transify/Transify/obj/Debug/net8.0/Transify.AssemblyInfo.cs
+++ Transify/Transify/obj/Debug/net8.0/Transify.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Transify")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad8433aaafb1aa25888f8e9bc021098f44aad54f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+711385341a71f6ab73786943670baf0a50e9dfe2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Transify")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Transify")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

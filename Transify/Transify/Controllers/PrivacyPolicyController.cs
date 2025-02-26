711385341a71f6ab73786943670baf0a50e9dfe2
GretaGashi <gg68557@ubt-uni.net>
2025-02-26 20:55:10 +0100

    PrivacyPolicyController

---
+++ Transify/Transify/Controllers/PrivacyPolicyController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Transify.Controllers
+{
+    [Route("PrivacyPolicy")]
+    public class PrivacyPolicyController : Controller
+    {
+
+        [HttpGet("", Name = "PrivacyPolicyIndex")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Transify/Transify/obj/Debug/net8.0/Transify.AssemblyInfo.cs
+++ Transify/Transify/obj/Debug/net8.0/Transify.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Transify")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+091df825bfcb99552f09505e19048c2794e34f33")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad8433aaafb1aa25888f8e9bc021098f44aad54f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Transify")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Transify")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
